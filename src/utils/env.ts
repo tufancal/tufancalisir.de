525af963db7a2961233c6b69da8d9af9f2fccfc3
Tufan Calisir <tufan.calisir@project-joker.com>
2025-12-28 21:24:43 +0100

    fix: use versioning util

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -3,8 +3,15 @@
  * - Dev server or staging build (--mode development): returns 'draft'
  * - Production build (--mode production): returns 'published'
  */
+<<<<<<< HEAD
 export const getStoryblokVersion = (): "draft" | "published" => {
   return import.meta.env.DEV || import.meta.env.MODE === "development"
     ? "draft"
     : "published";
+=======
+export const getStoryblokVersion = (): 'draft' | 'published' => {
+  return import.meta.env.DEV || import.meta.env.MODE === 'development'
+    ? 'draft'
+    : 'published';
+>>>>>>> ed2f3cf (fix: use versioning util)
 };
