0a8729fbe4fb26aff343b1afc4d07ff6c8839709
Tufan Calisir <98693559+tufancal@users.noreply.github.com>
2025-12-29 16:34:46 +0100

    fix: deployment error (#120)
    * fix: deployment fix
    
    * fix: cloudflare adapter for staging
    
    * fix: use versioning util
    
    * fix: deployment fix

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -3,8 +3,8 @@
  * - Dev server or staging build (--mode development): returns 'draft'
  * - Production build (--mode production): returns 'published'
  */
-export const getStoryblokVersion = (): 'draft' | 'published' => {
-  return import.meta.env.DEV || import.meta.env.MODE === 'development'
-    ? 'draft'
-    : 'published';
+export const getStoryblokVersion = (): "draft" | "published" => {
+  return import.meta.env.DEV || import.meta.env.MODE === "development"
+    ? "draft"
+    : "published";
 };
